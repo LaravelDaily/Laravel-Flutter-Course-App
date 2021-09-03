b744336f1522ec2a8570ee5ca599c196eab198bd
PovilasKorop <povilask007@gmail.com>
2021-09-03 19:57:12 +0300

    First lesson - Hello world in center

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text(
+              'Hello World',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)
+          ),
+        ),
+      ),
+    );
+  }
+}
