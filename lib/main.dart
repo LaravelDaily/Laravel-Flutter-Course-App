010595029620b19f805621088d2ca3b1f6e35ad3
PovilasKorop <povilask007@gmail.com>
2021-09-04 15:46:22 +0300

    Styled log in form

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,45 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Login'),
         ),
-        body: Column(
-          children: <Widget>[
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: 'Email',
-              ),
+        body: Container(
+          color: Theme.of(context).primaryColorDark,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Card(
+                    elevation: 8,
+                    margin: EdgeInsets.only(left: 16, right: 16),
+                    child: Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: <Widget>[
+                          TextField(
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                            ),
+                          ),
+                          TextField(
+                            keyboardType: TextInputType.visiblePassword,
+                            decoration: InputDecoration(labelText: 'Password'),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              print('Login attempt');
+                            },
+                            child: Text('Login'),
+                            style: ElevatedButton.styleFrom(
+                                minimumSize: Size(double.infinity, 36)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ]
             ),
-            TextField(
-              keyboardType: TextInputType.visiblePassword,
-              decoration: InputDecoration(
-                labelText: 'Password'
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () { print('Login attempt'); },
-              child: Text('Login'),
-              style: ElevatedButton.styleFrom(
-                  minimumSize: Size(double.infinity, 36)),
-            ),
-          ],
+          ),
         ),
       ),
     );
