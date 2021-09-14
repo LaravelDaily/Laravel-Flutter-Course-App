d87d27ab21eb7dc9c4af11819163583fc3baee4f
PovilasKorop <povilask007@gmail.com>
2021-09-14 09:04:32 +0300

    Move categories.dart to screens subfolder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_app/categories.dart';
+import 'package:my_first_app/screens/categories.dart';
 import 'package:my_first_app/screens/login.dart';
 import 'package:my_first_app/screens/register.dart';
 
